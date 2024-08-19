1bb9925598ff1404936fd28d837572b1c8ff2946
DanielStein <daniel.cloudware@gmail.com>
2024-08-19 11:20:15 -0700

    many to many relationship review

--- src/main/resources/db/migration/V1__BookTable.sql
+++ src/main/resources/db/migration/V1__BookTable.sql
@@ -1,14 +1,29 @@
+-- Create a new database clap_db (if it doesn't exist)
+CREATE DATABASE IF NOT EXISTS clap_db;
+
+-- Use the newly created clap_db database
+USE clap_db;
+
 CREATE TABLE book
 (
- book_id UUID NOT NULL,
- book_type varchar(100) NOT NULL,
+ bookId UUID NOT NULL,
+ bookType varchar(100) NOT NULL,
  authorId varchar(100) NOT NULL,
  book_reference varchar(100) NOT NULL,
  numberOfPages int,
  totalNumberOfPages int,
+ published boolean;
 );
 
 CREATE TABLE author (
  authorId int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ firstName varchar(100) NOT NULL,
+ lastName varchar(100) NOT NULL,
  authorName varchar(100) NOT NULL,
+);
+
+CREATE TABLE book_authors (
+  PRIMARY KEY (bookId, authorId),
+  FOREIGN KEY (bookId) REFERENCES Book(bookId),
+  FOREIGN KEY (authorId) REFERENCES Author(AuthorId)
 );
